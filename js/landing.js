0aae205b06c547bd212375fa46afc7502ddf3e65
Neeraj Rawat <neerajrawatnt@gmail.com>
2021-03-09 19:40:36 +0530

    booking container

--- js/landing.js
+++ js/landing.js
@@ -1,35 +1,14 @@
 window.addEventListener('load', () => {
-  let moreTravel = document
-    .getElementById('moreTravel')
-    .addEventListener('click', showSubMenu);
-
-  let sigbInDiv = document
-    .querySelector('.signinDiv')
-    .addEventListener('click', showSignIn);
+  let bookingSlot = document.querySelectorAll('.bookingIcons > li');
+  console.log(bookingSlot);
+  bookingSlot.forEach((el) => {
+    el.addEventListener('click', changeBookingSlot);
+  });
 });
 
-var submenuCounter = 0;
-
-var showSubMenu = () => {
-  let submenu = document.querySelector('.submenu');
-  if (submenuCounter % 2 == 0) {
-    submenu.style.display = 'block';
-    submenuCounter++;
-  } else {
-    submenu.style.display = 'none';
-    submenuCounter++;
-  }
-};
-
-var signInCounter = 0;
-
-var showSignIn = () => {
-  let signIn = document.querySelector('.signin');
-  if (signInCounter % 2 == 0) {
-    signIn.style.display = 'block';
-    signInCounter++;
-  } else {
-    signIn.style.display = 'none';
-    signInCounter++;
-  }
+var changeBookingSlot = (event) => {
+  let currentBooking = document.querySelector('.currentBooking');
+  currentBooking.classList.remove('currentBooking');
+  console.log(event.target);
+  event.target.classList.add('currentBooking');
 };
