bb952f9719e9e28b02bf6208d8aa4e0249736c56
Neeraj Rawat <neerajrawatnt@gmail.com>
2021-03-10 16:28:30 +0530

    booking container inputs added

--- js/landing.js
+++ js/landing.js
@@ -1,46 +1,65 @@
 window.addEventListener('load', () => {
   let bookingSlot = document.querySelectorAll('.bookingIcons > li');
-  console.log(bookingSlot);
   bookingSlot.forEach((el) => {
     el.addEventListener('click', changeBookingSlot);
   });
 
-  let searchBtn = document
-    .querySelector('.searchBtn')
+  let clickToSearchLocation = document
+    .querySelector('.clickToSearchLocation')
     .addEventListener('click', showsearchLocationDiv);
 
-  let travellersBtn = document
-    .querySelector('.travellersBtn ')
+  let clickToAddTravellers = document
+    .querySelector('.clickToAddTravellers ')
     .addEventListener('click', showtravellersDetailDiv);
 });
 
 var changeBookingSlot = (event) => {
   let currentBooking = document.querySelector('.currentBooking');
   currentBooking.classList.remove('currentBooking');
-  console.log(event.target);
+  // console.log(event.target);
   event.target.classList.add('currentBooking');
+  let dataKey = event.target.getAttribute('data-key');
+  showbookingDetailShow(dataKey);
 };
 
 let showsearchLocationDivCounter = 0;
 var showsearchLocationDiv = () => {
   let searchLocationDiv = document.querySelector('.searchLocationDiv');
+  // console.log(searchLocationDiv);
   if (showsearchLocationDivCounter % 2 == 0) {
     searchLocationDiv.style.display = 'block';
     showsearchLocationDivCounter++;
-  } else {
-    searchLocationDiv.style.display = 'none';
-    showsearchLocationDivCounter++;
   }
+  // else {
+  //   searchLocationDiv.style.display = 'none';
+  //   showsearchLocationDivCounter++;
+  // }
 };
 
 let showtravellersDetailDivCounter = 0;
 var showtravellersDetailDiv = () => {
   let travellersDetailDiv = document.querySelector('.travellersDetailDiv');
+  console.log(travellersDetailDiv);
   if (showtravellersDetailDivCounter % 2 == 0) {
     travellersDetailDiv.style.display = 'block';
     showtravellersDetailDivCounter++;
-  } else {
-    travellersDetailDiv.style.display = 'none';
-    showtravellersDetailDivCounter++;
   }
+  // else {
+  //   travellersDetailDiv.style.display = 'none';
+  //   showtravellersDetailDivCounter++;
+  // }
+};
+
+var showbookingDetailShow = (dataKey) => {
+  let bookingDetailShowAll = document.querySelectorAll('.bookingDetailShow');
+  // console.log(bookingDetailShowAll);
+
+  bookingDetailShowAll.forEach((el) => {
+    let dataKey2 = el.getAttribute('data-key');
+    if (dataKey == dataKey2) {
+      el.classList.add('displayDetails');
+    } else {
+      el.classList.remove('displayDetails');
+    }
+  });
 };
