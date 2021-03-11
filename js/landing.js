aef0451daf724cdea1a252da904e158e8c2bbdd4
Neeraj Rawat <neerajrawatnt@gmail.com>
2021-03-11 10:57:11 +0530

    search and add traveller eventlistner added

--- js/landing.js
+++ js/landing.js
@@ -31,17 +31,16 @@ var changeBookingSlot = (event) => {
 };
 
 let showsearchLocationDivCounter = 0;
-var showsearchLocationDiv = () => {
+var showsearchLocationDiv = (i) => {
   let searchLocationDiv = document.querySelector('.searchLocationDiv');
   // console.log(searchLocationDiv);
-  if (showsearchLocationDivCounter % 2 == 0) {
+  if (showsearchLocationDivCounter % 2 == 0 && i != 1) {
     searchLocationDiv.style.display = 'block';
     showsearchLocationDivCounter++;
+  } else if (showsearchLocationDivCounter % 2 == 1 || i == 1) {
+    searchLocationDiv.style.display = 'none';
+    showsearchLocationDivCounter++;
   }
-  // else {
-  //   searchLocationDiv.style.display = 'none';
-  //   showsearchLocationDivCounter++;
-  // }
 };
 
 let showtravellersDetailDivCounter = 0;
@@ -51,6 +50,7 @@ var showtravellersDetailDiv = () => {
   if (showtravellersDetailDivCounter % 2 == 0) {
     travellersDetailDiv.style.display = 'block';
     showtravellersDetailDivCounter++;
+    showsearchLocationDiv(1);
   } else {
     travellersDetailDiv.style.display = 'none';
     showtravellersDetailDivCounter++;
